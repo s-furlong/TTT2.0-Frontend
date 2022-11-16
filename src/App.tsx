62256f3cb59736efb2728f951e98402452ef937e
s-furlong <sfurlong@8thlight.com>
2022-11-16 11:18:03 -0500

    Compoenent for game created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import Games from "./components/games";
+import { useEffect, useState } from "react";
+
+const API_URL = "http://localhost:3000/api/v1/games";
+
+function getAPIData() {
+  return axios.get(API_URL).then((response) => response.data);
+}
 
 function App() {
+  const [games, setGame] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getAPIData().then((items) => {
+      if (mounted) {
+        setGame(items);
+      }
+    });
+    return () => (mounted = false);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TTT 2.0</h1>
+      <Games games={games} />
     </div>
   );
 }
