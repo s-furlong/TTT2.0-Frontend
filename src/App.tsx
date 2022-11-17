282e1afb9b28a3e21558f5cf66cdd5c078b7869b
s-furlong <sfurlong@8thlight.com>
2022-11-17 12:19:25 -0500

    URL added to axios

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import Games from "./components/games";
 import { useEffect, useState } from "react";
 
-const API_URL = "http://localhost:3000/api/v1/games";
+const API_URL = "http://127.0.0.1:3000/api/v1/games";
 
 function getAPIData() {
   return axios.get(API_URL).then((response) => response.data);
