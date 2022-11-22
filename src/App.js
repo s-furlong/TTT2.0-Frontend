5fe9bfb3f9109fad5d03b713b6317bf6c20298bd
s-furlong <sfurlong@8thlight.com>
2022-11-22 17:34:35 -0500

    added post request to the root app function

--- src/App.js
+++ src/App.js
@@ -4,48 +4,50 @@ import Games from "./components/games";
 import { useEffect, useState } from "react";
 import { Board } from "./components/board/board";
 
-const API_URL = "http://127.0.0.1:3000/api/v1/games";
 
-async function postAPIData(board) {
-  // try {
-    const { data } = await axios.post(
-      API_URL,
-      { board },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-      },
-    );
-
-    console.log(JSON.stringify(data, null, 4));
-
-    return data;
-  // } catch (error) {
-  //   if (axios.isAxiosError(error)) {
-  //     console.log('error message: ', error.message);
-  //     return error.message;
-  //   } else {
-  //     console.log('unexpected error: ', error);
-  //     return 'An unexpected error occurred';
-  //   }
-}
 
 function App() {
-  const [games, setGame] = useState([]);
+  const [games, setGame] = useState({});
 
-  // useEffect(() => {
-  //   getAPIData().then((items) => {
-  //       setGame(items);
-  //   });
-  // }, []);
+  const API_URL = "http://127.0.0.1:3000/api/v1/games";
+
+  async function postAPIData(board) {
+    // try {
+      const { data } = await axios.post(
+        API_URL,
+        { board },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+        },
+      );
+
+      setGame(data)
+
+    // } catch (error) {
+    //   if (axios.isAxiosError(error)) {
+    //     console.log('error message: ', error.message);
+    //     return error.message;
+    //   } else {
+    //     console.log('unexpected error: ', error);
+    //     return 'An unexpected error occurred';
+    //   }
+  }
+    
+
+    // useEffect(() => {
+    //   getAPIData().then((items) => {
+    //       setGame(items);
+    //   });
+    // }, []);
 
   return (
     <div className="App">
       <h1>TTT 2.0</h1>
       <Board postAPIData={postAPIData}/>
-      <Games games={games} />
+      <div>{ games.token }</div>
     </div>
   );
 }
