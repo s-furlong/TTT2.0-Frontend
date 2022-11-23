59c4d38c892b36b398deeaa85a284c3c385d61ad
s-furlong <sfurlong@8thlight.com>
2022-11-23 17:31:07 -0500

    new tests pass

--- src/components/GameStatus/gameStatus.test.js
+++ src/components/GameStatus/gameStatus.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import GameStatus from './gameStatus';
 
 describe('GameStatus', () => {
